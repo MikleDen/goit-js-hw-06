93757c088d442fcb24eebc546feb78fbc17c086d
Mykhailo Den <95014048+MikleDen@users.noreply.github.com>
2022-12-27 16:59:11 +0200

    Update task-02

--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,21 @@
+
+const ingr = document.querySelector("#ingredients");
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
 ];
+
+for (let i = 0; i < ingredients.length; i++) {
+  const ingredient = document.createElement("li");
+  ingredient.classList.add("item");
+  ingredient.textContent = ingredients[i];
+  
+  ingr.appendChild(ingredient);
+ 
+};
+
+
