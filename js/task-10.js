c426ff27b3ffec7fbf9db0742321d31ede1068f1
Mykhailo Den <95014048+MikleDen@users.noreply.github.com>
2022-12-30 16:35:33 +0200

    Update task-10.js

--- js/task-10.js
+++ js/task-10.js
@@ -1,3 +1,41 @@
+const btnCreateEl = document.querySelector('#controls button[data-create]');
+
+const btnDestroyEl = document.querySelector('#controls button[data-destroy]');
+
+const inputEl = document.querySelector('#controls input')
+
+const boxesEl = document.querySelector('#boxes');
+
+const controlsEl = document.querySelector('#controls');
+
+btnCreateEl.addEventListener('click',createEl);
+btnDestroyEl.addEventListener('click',destroyEl);
+
+function createEl(event){
+  createBoxes(Number(inputEl.value));
+}
+
+function destroyEl(event){
+  boxesEl.innerHTML=" ";
+  inputEl.value=0;
+}
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+function createBoxes(amount) {
+  let elements = ``;
+
+  for (let i = 0; i < amount; i += 1) {
+    elements += `<div style="background-color: ${getRandomHexColor()}"></div>`;
+  }
+  
+  boxesEl.insertAdjacentHTML('afterbegin', elements);
+
+  const arr = [...boxesEl.children];
+  arr.forEach((div, index) => {
+    div.style.width = `${30 + 10 * (index + 1)}px`;
+    div.style.height = `${30 + 10 * (index + 1)}px`;
+  });
+};
