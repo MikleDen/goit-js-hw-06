5537255d15d0f3f0f71c9c950a98f1eba56929b1
Mykhailo Den <95014048+MikleDen@users.noreply.github.com>
2022-12-30 16:12:10 +0200

    Update task-09.js

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,18 @@
+const color = document.querySelector('.color');
+const changeColorBtn = document.querySelector('.change-color');
+const body = document.querySelector('body');
+
+changeColorBtn.addEventListener('click',colorChange);
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+};
+
+function colorChange(event){
+
+  body.style.backgroundColor=getRandomHexColor();
+  color.innerHTML=(`${getRandomHexColor()}`)
+
 }
+
+
